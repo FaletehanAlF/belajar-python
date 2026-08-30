a5c3e16dcacf892968358d945ff84d9745c70024
FaletehanAlF <faletehanalfarabi09>
2026-08-30 21:59:00 +0700

    feat: add Python conditionals exercise

---
+++ conditionals.py
@@ -0,0 +1,11 @@
+nilai = 75
+
+
+if nilai >= 90:
+    print("Nilai kamu A")
+elif nilai >= 80:
+    print("Nilai kamu B")
+elif nilai >= 70:
+    print("Nilai kamu C")
+else:
+    print("Kamu perlu belajar lagi")
