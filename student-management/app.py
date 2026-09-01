96121c1cc57fd7406064a29f6676e78d35eb4967
FaletehanAlF <faletehanalfarabi09>
2026-09-02 06:48:03 +0700

    update project

--- student-management/app.py
+++ student-management/app.py
@@ -4,7 +4,6 @@ with open("students.json", "r") as file:
     students = json.load(file)
 
 for student in students:
-    print(f"ID     : {student['id']}")
     print(f"Nama   : {student['nama']}")
     print(f"Kelas  : {student['kelas']}")
     print(f"Umur   : {student['umur']}")
