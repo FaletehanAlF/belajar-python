d3b700ba5ef2ff447d1eff47379930a20ac21c4d
FaletehanAlF <faletehanalfarabi09>
2026-09-02 07:01:03 +0700

    update project

--- student-management/app.py
+++ student-management/app.py
@@ -3,9 +3,21 @@ import json
 with open("students.json", "r") as file:
     students = json.load(file)
 
-for student in students:
-    print(f"Nama   : {student['nama']}")
-    print(f"Kelas  : {student['kelas']}")
-    print(f"Umur   : {student['umur']}")
-    print(f"Alamat : {student['alamat']}")
-    print("-" * 30)
+
+nama = input("Masukkan nama: ")
+kelas = input("Masukkan kelas: ")
+umur = int(input("Masukkan umur: "))
+
+new_student = {
+    "id": len(students) + 1,
+    "nama": nama,
+    "kelas": kelas,
+    "umur": umur
+}
+
+students.append(new_student)
+
+with open("students.json", "w") as file:
+    json.dump(students, file, indent=4)
+
+print("Siswa berhasil ditambahkan!")
